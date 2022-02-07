5304564d661046147bff2b5533d7109edd99c219
Petr Dvoracek <petr.dvoracek.ml@gmail.com>
2022-02-07 16:44:05 +0100

    commited at 17pm

---
+++ ols_functions.py
@@ -0,0 +1,9 @@
+import statsmodels.api as sm
+
+def simplemodel(df):
+    result = sm.OLS(df['price'], sm.add_constant(df.loc[:,df.columns != 'price'])).fit()
+    return result.summary()
+
+def modelparameters(df):
+    result = sm.OLS(df['price'], sm.add_constant(df.loc[:,df.columns != 'price'])).fit()
+    return result.params
